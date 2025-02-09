3631560d3900aee190713d5ee05289e5f1bfba73
HazimNabil <HazemNabil95@outlook.com>
2025-02-10 00:04:52 +0200

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SpectraSports extends StatelessWidget {
+  const SpectraSports({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spectra Sports',
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:spectra_sports/app.dart';
+
+void main() {
+  runApp(const SpectraSports());
+}
