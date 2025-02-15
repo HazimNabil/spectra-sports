f3670adbf31836e747a67df05c24e13fd5aada03
HazimNabil <HazemNabil95@outlook.com>
2025-02-16 01:18:14 +0200

    Add animation to splash view

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/core/utils/app_images.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  double _turns = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    rotate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset(
-        AppImages.imagesSplash,
+      child: AnimatedRotation(
+        turns: _turns,
+        duration: const Duration(seconds: 3),
+        child: Image.asset(
+          AppImages.imagesSplash,
+        ),
       ),
+    );
+  }
+
+  void rotate() {
+    Future.delayed(
+      const Duration(seconds: 0),
+      () => setState(() => _turns = 1),
     );
   }
 }
