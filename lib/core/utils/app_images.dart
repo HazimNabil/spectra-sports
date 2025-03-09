cee9d7c045ed8c0a8557b9fd1c4ee85f71b8ff8c
HazimNabil <HazemNabil95@outlook.com>
2025-03-09 23:32:02 +0200

    Add the rest of images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,5 +1,19 @@
 class AppImages {
   AppImages._();
 
-  static const imagesSplash = 'assets/images/splash.png';
+  static const imagesAppLogo = "assets/images/app_logo.svg";
+
+  static const imagesAttendance = "assets/images/attendance.svg";
+
+  static const imagesCalendar = "assets/images/calendar.svg";
+
+  static const imagesCoach = "assets/images/coach.svg";
+
+  static const imagesFootballPlayer = "assets/images/football_player.svg";
+
+  static const imagesNearby = "assets/images/nearby.svg";
+
+  static const imagesSplash = "assets/images/splash.png";
+
+  static const imagesStadium = "assets/images/stadium.svg";
 }
