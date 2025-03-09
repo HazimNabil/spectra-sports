efc4dcacb10e67a5b3a5e0370f1b18d8d0362542
HazimNabil <HazemNabil95@outlook.com>
2025-03-09 23:45:38 +0200

    Add app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const background = Color(0xF0F0F0F7);
+  static const input = Color(0xFFFFFFFF);
+  static const icons = Color(0xFF484C52);
+  static const hint = Color(0xFF828282);
+  static const buttons = Color(0xFF006FFD);
+  static const highlight = Color(0xFFFF5959);
+  static const text = Color(0xFF000000);
+}
