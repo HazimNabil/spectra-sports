5c22aa6711bd8b6ab1ff8df6705b9557095bf63a
HazimNabil <HazemNabil95@outlook.com>
2025-03-11 00:48:15 +0200

    Add navigation to login view

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:spectra_sports/core/routes/app_router.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
 
 import '../widgets/splash_view_body.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToLoginView();
+  }
+
+  void navigateToLoginView() {
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        context.pushReplacement(AppRouter.loginRoute);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
