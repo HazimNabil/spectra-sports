a26ab067c0917a8db453e28e93b8c40e4602440e
HazimNabil <HazemNabil95@outlook.com>
2025-03-11 00:53:47 +0200

    Add background color to app theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:spectra_sports/core/utils/app_colors.dart';
 
 import 'core/routes/app_router.dart';
 
@@ -15,6 +16,7 @@ class SpectraSports extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
         textTheme: GoogleFonts.interTextTheme(),
       ),
       routerConfig: AppRouter.router,
