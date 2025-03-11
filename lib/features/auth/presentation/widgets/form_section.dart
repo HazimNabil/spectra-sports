eaefc572e01266bb5d90db9ca3181b7f866bc322
HazimNabil <HazemNabil95@outlook.com>
2025-03-11 23:37:42 +0200

    Add email and password fields

--- lib/features/auth/presentation/widgets/form_section.dart
+++ lib/features/auth/presentation/widgets/form_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/core/utils/app_styles.dart';
+import 'package:spectra_sports/core/widgets/custom_text_field.dart';
 
 class FormSection extends StatelessWidget {
   const FormSection({super.key});
@@ -9,11 +10,16 @@ class FormSection extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             'Welcome!',
             style: AppStyles.styleExtraBold24(context),
           ),
+          const SizedBox(height: 24),
+          const CustomTextField(hintText: 'Email Address'),
+          const SizedBox(height: 16),
+          const CustomTextField(hintText: 'Password', isPasswordField: true),
         ],
       ),
     );
