61e3f975eb3ea2cf9a1f0f0eced52e33efb40bc3
HazimNabil <HazemNabil95@outlook.com>
2025-03-11 23:56:19 +0200

    Make logo section height responsive

--- lib/features/auth/presentation/widgets/logo_section.dart
+++ lib/features/auth/presentation/widgets/logo_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
 import 'package:spectra_sports/core/utils/app_images.dart';
+import 'package:spectra_sports/core/utils/extensions.dart';
 
 class LogoSection extends StatelessWidget {
   const LogoSection({super.key});
@@ -8,7 +9,7 @@ class LogoSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 312,
+      height: context.height * 0.38,
       width: double.infinity,
       color: AppColors.logoBackground,
       child: Center(
