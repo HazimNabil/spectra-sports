b9726d4331441600c0925ad6b9c07352cc0abf37
HazimNabil <HazemNabil95@outlook.com>
2025-03-12 17:18:39 +0200

    Add password fields

--- lib/features/auth/presentation/widgets/sign_up_form_section.dart
+++ lib/features/auth/presentation/widgets/sign_up_form_section.dart
@@ -22,7 +22,7 @@ class SignUpFormSection extends StatelessWidget {
         ),
         const SizedBox(height: 8),
         const CustomTextField(hintText: 'Name'),
-        const SizedBox(height: 16),
+        const SizedBox(height: 24),
         Text(
           'Email Address',
           style: AppStyles.styleSemiBold12(
@@ -34,7 +34,26 @@ class SignUpFormSection extends StatelessWidget {
         ),
         const SizedBox(height: 8),
         const CustomTextField(hintText: 'name@gmail.com'),
-        const SizedBox(height: 16),
+        const SizedBox(height: 24),
+        Text(
+          'Password',
+          style: AppStyles.styleSemiBold12(
+            context,
+          ).copyWith(
+            color: AppColors.text,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        const CustomTextField(
+          hintText: 'Create a password',
+          isPasswordField: true,
+        ),
+        const SizedBox(height: 24),
+        const CustomTextField(
+          hintText: 'Confirm password',
+          isPasswordField: true,
+        ),
       ],
     );
   }
