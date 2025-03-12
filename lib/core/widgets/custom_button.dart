3ffde6dd624d840dcfbba8e8a09bd7966b91d355
HazimNabil <HazemNabil95@outlook.com>
2025-03-12 14:48:07 +0200

    Create custom button

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:spectra_sports/core/utils/app_colors.dart';
+import 'package:spectra_sports/core/utils/app_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      style: FilledButton.styleFrom(
+        backgroundColor: AppColors.buttons,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        title,
+        style: AppStyles.styleSemiBold12(
+          context,
+        ).copyWith(color: AppColors.input),
+      ),
+    );
+  }
+}
