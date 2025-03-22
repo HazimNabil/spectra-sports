1c1f546bd5f03c7bf1e3b3e819c3b59a27a560f3
HazimNabil <HazemNabil95@outlook.com>
2025-03-22 17:18:04 +0200

    Refactor academy info form

--- lib/features/auth/presentation/widgets/academy_info_form.dart
+++ lib/features/auth/presentation/widgets/academy_info_form.dart
@@ -2,26 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
 import 'package:spectra_sports/core/utils/app_styles.dart';
 import 'package:spectra_sports/core/utils/app_validators.dart';
-import 'package:spectra_sports/core/utils/extensions.dart';
-import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/core/widgets/custom_text_field.dart';
 
-class AcademyInfoForm extends StatefulWidget {
-  const AcademyInfoForm({super.key});
+class AcademyInfoForm extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final AutovalidateMode autovalidateMode;
 
-  @override
-  State<AcademyInfoForm> createState() => _AcademyInfoFormState();
-}
-
-class _AcademyInfoFormState extends State<AcademyInfoForm> {
-  final _formKey = GlobalKey<FormState>();
-  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  const AcademyInfoForm({
+    super.key,
+    required this.formKey,
+    required this.autovalidateMode,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      autovalidateMode: _autovalidateMode,
+      key: formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -68,20 +65,6 @@ class _AcademyInfoFormState extends State<AcademyInfoForm> {
             hintText: '01077...',
             keyboardType: TextInputType.number,
             validator: AppValidators.phoneNumberValidator,
-          ),
-          SizedBox(height: context.height * 0.25),
-          SizedBox(
-            height: 48,
-            width: double.infinity,
-            child: CustomButton(
-              title: 'Save',
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                } else {
-                  setState(() => _autovalidateMode = AutovalidateMode.always);
-                }
-              },
-            ),
           ),
         ],
       ),
--- lib/features/auth/presentation/widgets/academy_info_view_body.dart
+++ lib/features/auth/presentation/widgets/academy_info_view_body.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
 import 'package:spectra_sports/core/utils/app_styles.dart';
+import 'package:spectra_sports/core/utils/extensions.dart';
+import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/features/auth/presentation/widgets/academy_info_form.dart';
 
-class AcademyInfoViewBody extends StatelessWidget {
+class AcademyInfoViewBody extends StatefulWidget {
   const AcademyInfoViewBody({super.key});
 
   @override
+  State<AcademyInfoViewBody> createState() => _AcademyInfoViewBodyState();
+}
+
+class _AcademyInfoViewBodyState extends State<AcademyInfoViewBody> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
+    return SingleChildScrollView(
       padding: const EdgeInsets.all(24),
-      child: SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minHeight: context.height * 0.87),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 48,
           children: [
             Text(
               'Academy Information',
@@ -21,7 +32,23 @@ class AcademyInfoViewBody extends StatelessWidget {
                 context,
               ).copyWith(color: AppColors.text),
             ),
-            const AcademyInfoForm(),
+            AcademyInfoForm(
+              formKey: _formKey,
+              autovalidateMode: _autovalidateMode,
+            ),
+            SizedBox(
+              height: 48,
+              width: double.infinity,
+              child: CustomButton(
+                title: 'Save',
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                  } else {
+                    setState(() => _autovalidateMode = AutovalidateMode.always);
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
