0226dad366803d6e8153d7125688ffea367422c8
HazimNabil <HazemNabil95@outlook.com>
2025-03-22 18:00:41 +0200

    Refactor login form

--- lib/features/auth/presentation/widgets/login_form_section.dart
+++ lib/features/auth/presentation/widgets/login_form_section.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/core/utils/app_styles.dart';
 import 'package:spectra_sports/core/utils/app_validators.dart';
-import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/core/widgets/custom_text_field.dart';
 
-class LoginFormSection extends StatefulWidget {
-  const LoginFormSection({super.key});
+class LoginFormSection extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final AutovalidateMode autovalidateMode;
 
-  @override
-  State<LoginFormSection> createState() => _LoginFormSectionState();
-}
-
-class _LoginFormSectionState extends State<LoginFormSection> {
-  final _formKey = GlobalKey<FormState>();
-  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  const LoginFormSection({
+    super.key,
+    required this.formKey,
+    required this.autovalidateMode,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Form(
-        autovalidateMode: _autovalidateMode,
-        key: _formKey,
+        autovalidateMode: autovalidateMode,
+        key: formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -41,19 +39,6 @@ class _LoginFormSectionState extends State<LoginFormSection> {
               validator: AppValidators.passwordValidator,
             ),
             const SizedBox(height: 55),
-            SizedBox(
-              height: 48,
-              width: double.infinity,
-              child: CustomButton(
-                title: 'Login',
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                  } else {
-                    setState(() => _autovalidateMode = AutovalidateMode.always);
-                  }
-                },
-              ),
-            ),
           ],
         ),
       ),
--- lib/features/auth/presentation/widgets/login_view_body.dart
+++ lib/features/auth/presentation/widgets/login_view_body.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/features/auth/presentation/widgets/login_form_section.dart';
 import 'package:spectra_sports/features/auth/presentation/widgets/logo_section.dart';
 import 'package:spectra_sports/features/auth/presentation/widgets/sign_up_prompt_text.dart';
 
-class LoginViewBody extends StatelessWidget {
+class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
 
   @override
+  State<LoginViewBody> createState() => _LoginViewBodyState();
+}
+
+class _LoginViewBodyState extends State<LoginViewBody> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+
+  @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          LogoSection(),
-          SizedBox(height: 40),
-          LoginFormSection(),
-          SizedBox(height: 24),
-          Align(
+          const LogoSection(),
+          const SizedBox(height: 40),
+          LoginFormSection(
+            formKey: _formKey,
+            autovalidateMode: _autovalidateMode,
+          ),
+          Container(
+            height: 48,
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            child: CustomButton(
+              title: 'Login',
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                } else {
+                  setState(() => _autovalidateMode = AutovalidateMode.always);
+                }
+              },
+            ),
+          ),
+          const SizedBox(height: 24),
+          const Align(
             alignment: Alignment.center,
             child: SignUpPromptText(),
           ),
+          const SizedBox(height: 16),
         ],
       ),
     );
