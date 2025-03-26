20401e43d8dd016cf707673d42cc1ce9b9d5314e
HazimNabil <HazemNabil95@outlook.com>
2025-03-27 01:23:35 +0200

    Add height and width scale factors

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -5,7 +5,7 @@ import 'package:spectra_sports/core/utils/extensions.dart';
 abstract class AppStyles {
   static TextStyle styleExtraBold24(BuildContext context) {
     return TextStyle(
-      fontSize: 24 * context.width / 375,
+      fontSize: 24 * context.widthScaleFactor,
       fontWeight: FontWeight.bold,
       color: AppColors.text,
     );
@@ -13,7 +13,7 @@ abstract class AppStyles {
 
   static TextStyle styleRegular14(BuildContext context) {
     return TextStyle(
-      fontSize: 14 * context.width / 375,
+      fontSize: 14 * context.widthScaleFactor,
       fontWeight: FontWeight.normal,
       color: AppColors.hint,
     );
@@ -21,7 +21,7 @@ abstract class AppStyles {
 
   static TextStyle styleBold20(BuildContext context) {
     return TextStyle(
-      fontSize: 20 * context.width / 375,
+      fontSize: 20 * context.widthScaleFactor,
       fontWeight: FontWeight.bold,
       color: AppColors.background,
     );
@@ -29,7 +29,7 @@ abstract class AppStyles {
 
   static TextStyle styleBold15(BuildContext context) {
     return TextStyle(
-      fontSize: 15 * context.width / 375,
+      fontSize: 15 * context.widthScaleFactor,
       fontWeight: FontWeight.bold,
       color: AppColors.text,
     );
@@ -37,7 +37,7 @@ abstract class AppStyles {
 
   static TextStyle styleSemiBold16(BuildContext context) {
     return TextStyle(
-      fontSize: 16 * context.width / 375,
+      fontSize: 16 * context.widthScaleFactor,
       fontWeight: FontWeight.w600,
       color: AppColors.highlight,
     );
@@ -45,7 +45,7 @@ abstract class AppStyles {
 
   static TextStyle styleSemiBold12(BuildContext context) {
     return TextStyle(
-      fontSize: 12 * context.width / 375,
+      fontSize: 12 * context.widthScaleFactor,
       fontWeight: FontWeight.w600,
       color: AppColors.buttons,
     );
--- lib/core/utils/extensions.dart
+++ lib/core/utils/extensions.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 extension MediaQueryHelper on BuildContext {
   double get height => MediaQuery.sizeOf(this).height;
   double get width => MediaQuery.sizeOf(this).width;
+  double get heightScaleFactor => height / 812;
+  double get widthScaleFactor => width / 375;
 }
