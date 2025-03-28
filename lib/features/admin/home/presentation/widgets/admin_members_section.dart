2ef05bf698e82821c84ce01a795863903d949492
HazimNabil <HazemNabil95@outlook.com>
2025-03-29 00:28:26 +0200

    Create player card

--- lib/features/admin/home/presentation/widgets/admin_members_section.dart
+++ lib/features/admin/home/presentation/widgets/admin_members_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/features/admin/home/presentation/widgets/coach_card.dart';
+import 'package:spectra_sports/features/admin/home/presentation/widgets/player_card.dart';
 
 class AdminMembersSection extends StatelessWidget {
   const AdminMembersSection({super.key});
@@ -10,8 +11,10 @@ class AdminMembersSection extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: ListView(
         padding: EdgeInsets.zero,
-        children: const [
-          CoachCard(),
+        children: [
+          const CoachCard(),
+          const SizedBox(height: 12),
+          ...List.generate(11, (_) => const PlayerCard()),
         ],
       ),
     );
---
+++ lib/features/admin/home/presentation/widgets/player_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:spectra_sports/core/utils/app_colors.dart';
+import 'package:spectra_sports/core/utils/app_styles.dart';
+
+class PlayerCard extends StatelessWidget {
+  const PlayerCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.background,
+      elevation: 4,
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        leading: const CircleAvatar(
+          radius: 25,
+          backgroundColor: AppColors.text,
+          backgroundImage: NetworkImage(
+            'https://cdn.sofifa.net/players/242/516/22_120.png',
+          ),
+        ),
+        title: Text(
+          'Cody Gakpo',
+          style: AppStyles.styleSemiBold16(
+            context,
+          ).copyWith(color: AppColors.icons),
+        ),
+      ),
+    );
+  }
+}
