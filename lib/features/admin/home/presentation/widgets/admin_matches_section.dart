5bd84812024a69eb36ed8f5d48500c4fee7f793e
HazimNabil <HazemNabil95@outlook.com>
2025-03-28 22:52:53 +0200

    Add add match button

--- lib/features/admin/home/presentation/widgets/admin_matches_section.dart
+++ lib/features/admin/home/presentation/widgets/admin_matches_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spectra_sports/core/utils/app_colors.dart';
+import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/features/admin/home/presentation/widgets/admin_match_card_list_view.dart';
 
 class AdminMatchesSection extends StatelessWidget {
@@ -6,13 +8,22 @@ class AdminMatchesSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       spacing: 16,
       children: [
-        Expanded(
+        const Expanded(
           child: AdminMatchCardListView(),
         ),
-        SizedBox(height: 0),
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(horizontal: 48),
+          child: CustomButton(
+            title: 'Add Match',
+            color: AppColors.highlight,
+            onPressed: () {},
+          ),
+        ),
+        const SizedBox(height: 0),
       ],
     );
   }
