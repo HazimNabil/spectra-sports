90a964358d95aa1fb7da8cb79aed7f4de19285e0
HazimNabil <HazemNabil95@outlook.com>
2025-03-31 18:21:39 +0200

    Setup service locator

---
+++ lib/core/di/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:spectra_sports/core/network/api_service.dart';
+import 'package:spectra_sports/features/auth/data/repos/auth_repo_impl.dart';
+
+final locator = GetIt.instance;
+
+void setupServiceLocator() {
+  locator.registerSingleton(Dio());
+
+  locator.registerLazySingleton(
+    () => ApiService(locator<Dio>()),
+  );
+
+  locator.registerLazySingleton(
+    () => AuthRepoImpl(locator<ApiService>()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/app.dart';
+import 'package:spectra_sports/core/di/service_locator.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
