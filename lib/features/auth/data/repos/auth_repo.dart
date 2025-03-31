25e2be7327329245cb8702d59e5ed8eeb24b4d6d
HazimNabil <HazemNabil95@outlook.com>
2025-03-31 11:37:37 +0200

    Create auth repository interface

---
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:spectra_sports/core/utils/typedefs.dart';
+import 'package:spectra_sports/features/auth/data/models/login_body_model.dart';
+import 'package:spectra_sports/features/auth/data/models/sign_up_body_model.dart';
+
+abstract interface class AuthRepo {
+  ApiResult<Unit> login(LoginBodyModel loginBody);
+  ApiResult<Unit> signUp(SignUpBodyModel signUpBody);
+}
