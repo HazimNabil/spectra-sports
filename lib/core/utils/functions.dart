028ea17206997cf13014b60aeea36f10ce59de4d
HazimNabil <HazemNabil95@outlook.com>
2025-04-01 01:23:29 +0200

    Define show toast and navigate by role as helper functions

---
+++ lib/core/utils/functions.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:spectra_sports/core/routes/app_router.dart';
+import 'package:toastification/toastification.dart';
+
+void navigateByRole(String role, BuildContext context) {
+  switch (role) {
+    case 'admin':
+      context.go(AppRouter.adminHomeRoute);
+    case 'coach':
+      context.go(AppRouter.coachHomeRoute);
+    case 'parent':
+      context.go(AppRouter.parentHomeRoute);
+  }
+}
+
+void showToast({
+  required BuildContext context,
+  required String title,
+  required ToastificationType type,
+}) {
+  toastification.show(
+    context: context,
+    title: Text(title),
+    type: type,
+    style: ToastificationStyle.minimal,
+  );
+}
