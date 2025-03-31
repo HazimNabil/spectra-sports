35183b9244ce4554e5d9a3dbd13aa708dac71f95
HazimNabil <HazemNabil95@outlook.com>
2025-03-31 21:23:55 +0200

    Modify auth repo to return user model

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,11 +1,11 @@
-import 'package:dartz/dartz.dart';
 import 'package:spectra_sports/core/utils/typedefs.dart';
 import 'package:spectra_sports/features/auth/data/models/login_body_model.dart';
 import 'package:spectra_sports/features/auth/data/models/sign_up_body_model.dart';
+import 'package:spectra_sports/features/auth/data/models/user_model.dart';
 
 abstract interface class AuthRepo {
-  ApiResult<Unit> login(LoginBodyModel loginBody);
-  ApiResult<Unit> signUp(SignUpBodyModel signUpBody);
+  ApiResult<UserModel> login(LoginBodyModel loginBody);
+  ApiResult<UserModel> signUp(SignUpBodyModel signUpBody);
   Future<void> logout();
-  Future<bool> checkAuthStatus();
+  Future<UserModel?> checkAuthStatus();
 }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -4,9 +4,11 @@ import 'package:spectra_sports/core/errors/server_failure.dart';
 import 'package:spectra_sports/core/network/api_constants.dart';
 import 'package:spectra_sports/core/network/api_service.dart';
 import 'package:spectra_sports/core/utils/cache_helper.dart';
+import 'package:spectra_sports/core/utils/token_decoder.dart';
 import 'package:spectra_sports/core/utils/typedefs.dart';
 import 'package:spectra_sports/features/auth/data/models/login_body_model.dart';
 import 'package:spectra_sports/features/auth/data/models/sign_up_body_model.dart';
+import 'package:spectra_sports/features/auth/data/models/user_model.dart';
 import 'package:spectra_sports/features/auth/data/repos/auth_repo.dart';
 
 class AuthRepoImpl implements AuthRepo {
@@ -15,17 +17,22 @@ class AuthRepoImpl implements AuthRepo {
   AuthRepoImpl(this._apiService);
 
   @override
-  ApiResult<Unit> login(LoginBodyModel loginBody) async {
+  ApiResult<UserModel> login(LoginBodyModel loginBody) async {
     try {
       final jsonData = await _apiService.post(
         ApiConstants.login,
         loginBody.toJson(),
       );
+
+      final jsonUser = TokenDecoder.decode(jsonData[ApiConstants.tokenKey]);
+      final user = UserModel.fromJson(jsonUser);
+
       await CacheHelper.setSecureData(
         ApiConstants.tokenKey,
         jsonData[ApiConstants.tokenKey],
       );
-      return const Right(unit);
+
+      return Right(user);
     } on DioException catch (e) {
       return Left(
         ServerFailure.fromDioException(e),
@@ -38,17 +45,22 @@ class AuthRepoImpl implements AuthRepo {
   }
 
   @override
-  ApiResult<Unit> signUp(SignUpBodyModel signUpBody) async {
+  ApiResult<UserModel> signUp(SignUpBodyModel signUpBody) async {
     try {
       final jsonData = await _apiService.post(
         ApiConstants.login,
         signUpBody.toJson(),
       );
+
+      final jsonUser = TokenDecoder.decode(jsonData[ApiConstants.tokenKey]);
+      final user = UserModel.fromJson(jsonUser);
+
       await CacheHelper.setSecureData(
         ApiConstants.tokenKey,
         jsonData[ApiConstants.tokenKey],
       );
-      return const Right(unit);
+
+      return Right(user);
     } on DioException catch (e) {
       return Left(
         ServerFailure.fromDioException(e),
@@ -66,8 +78,12 @@ class AuthRepoImpl implements AuthRepo {
   }
 
   @override
-  Future<bool> checkAuthStatus() async {
+  Future<UserModel?> checkAuthStatus() async {
     final token = await CacheHelper.getSecureData(ApiConstants.tokenKey);
-    return token.isNotEmpty;
+
+    if (token.isEmpty) return null;
+
+    final jsonUser = TokenDecoder.decode(token);
+    return UserModel.fromJson(jsonUser);
   }
 }
