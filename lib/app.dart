8d996a6ff95d6ca7b90093fe29114177f47c9995
HazimNabil <HazemNabil95@outlook.com>
2025-04-01 21:07:50 +0200

    Fix issue with locator type for auth repo implementation

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:spectra_sports/core/di/service_locator.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
-import 'package:spectra_sports/features/auth/data/repos/auth_repo.dart';
+import 'package:spectra_sports/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:spectra_sports/features/auth/presentation/view_models/auth_cubit/auth_cubit.dart';
 
 import 'core/routes/app_router.dart';
@@ -15,7 +15,7 @@ class SpectraSports extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AuthCubit(locator<AuthRepo>()),
+      create: (context) => AuthCubit(locator<AuthRepoImpl>()),
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Spectra Sports',
