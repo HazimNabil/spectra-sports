a8c4140052e0c9092ee27a39db0233b11123d946
HazimNabil <HazemNabil95@outlook.com>
2025-04-01 21:09:22 +0200

    Use and trigger app bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spectra_sports/app.dart';
 import 'package:spectra_sports/core/di/service_locator.dart';
+import 'package:spectra_sports/core/utils/app_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   setupServiceLocator();
   runApp(
     DevicePreview(
-      enabled: !kReleaseMode,
+      enabled: false,
       builder: (context) => const SpectraSports(),
     ),
   );
