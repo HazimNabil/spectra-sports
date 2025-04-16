a57a2418c8ade23f57e832ca1ab67f1959940e9c
HazimNabil <HazemNabil95@outlook.com>
2025-04-16 22:38:31 +0200

    Add add match method to add match cubit

--- lib/features/admin/home/presentation/view_models/add_match_cubit/add_match_cubit.dart
+++ lib/features/admin/home/presentation/view_models/add_match_cubit/add_match_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spectra_sports/core/models/match_model.dart';
+import 'package:spectra_sports/features/admin/home/data/repos/admin_home_repo.dart';
 
 part 'add_match_state.dart';
 
 class AddMatchCubit extends Cubit<AddMatchState> {
-  AddMatchCubit() : super(const AddMatchInitial());
+  final AdminHomeRepo _adminHomeRepo;
+
+  AddMatchCubit(this._adminHomeRepo) : super(const AddMatchInitial());
+
+  Future<void> addMatch(MatchModel match) async {
+    emit(const AddMatchLoading());
+
+    final matchEither = await _adminHomeRepo.addMatch(match);
+    matchEither.fold(
+      (failure) => emit(AddMatchFailure(failure.message)),
+      (_) => emit(const AddMatchSuccess()),
+    );
+  }
 }
