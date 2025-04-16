f66977a3d270c519a9dfd658462be39f5ee03c98
HazimNabil <HazemNabil95@outlook.com>
2025-04-16 02:37:44 +0200

    Define get teams cubit method

--- lib/features/admin/home/presentation/view_models/get_teams_cubit/get_teams_cubit.dart
+++ lib/features/admin/home/presentation/view_models/get_teams_cubit/get_teams_cubit.dart
@@ -2,9 +2,22 @@ import 'package:flutter/foundation.dart' show immutable;
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spectra_sports/core/models/team.dart';
+import 'package:spectra_sports/features/admin/home/data/repos/admin_home_repo.dart';
 
 part 'get_teams_state.dart';
 
 class GetTeamsCubit extends Cubit<GetTeamsState> {
-  GetTeamsCubit() : super(const GetTeamsInitial());
+  final AdminHomeRepo _adminHomeRepo;
+
+  GetTeamsCubit(this._adminHomeRepo) : super(const GetTeamsInitial());
+
+  Future<void> getTeams() async {
+    emit(const GetTeamsLoading());
+
+    final teamsEither = await _adminHomeRepo.getTeams();
+    teamsEither.fold(
+      (failure) => emit(GetTeamsFailure(failure.message)),
+      (teams) => emit(GetTeamsSuccess(teams)),
+    );
+  }
 }
