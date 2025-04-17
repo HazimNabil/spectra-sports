e50641db1698d0e2259819f1c8c6a3ef6d5dbadc
HazimNabil <HazemNabil95@outlook.com>
2025-04-17 17:05:37 +0200

    Integrate and trigger add match cubit

--- lib/features/admin/home/presentation/widgets/add_match_view_body.dart
+++ lib/features/admin/home/presentation/widgets/add_match_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:spectra_sports/core/models/match_model.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
 import 'package:spectra_sports/core/utils/app_styles.dart';
+import 'package:spectra_sports/core/utils/extensions.dart';
+import 'package:spectra_sports/core/utils/functions.dart';
 import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/features/admin/home/data/models/add_match_input.dart';
+import 'package:spectra_sports/features/admin/home/presentation/view_models/add_match_cubit/add_match_cubit.dart';
 import 'package:spectra_sports/features/admin/home/presentation/widgets/add_match_form.dart';
+import 'package:toastification/toastification.dart';
 
 class AddMatchViewBody extends StatefulWidget {
   final String teamId;
@@ -54,21 +60,40 @@ class _AddMatchViewBodyState extends State<AddMatchViewBody> {
                   );
                 },
               ),
-              const SizedBox.expand(),
-              SizedBox(
-                width: double.infinity,
-                child: CustomButton(
-                  title: 'Add',
-                  color: AppColors.highlight,
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      MatchModel.fromAddMatchInput(_addMatchInput);
-                    } else {
-                      _autovalidateMode.value = AutovalidateMode.always;
-                    }
-                  },
-                ),
+              SizedBox(height: context.height * 0.2),
+              BlocConsumer<AddMatchCubit, AddMatchState>(
+                listener: (context, state) {
+                  if (state is AddMatchSuccess) {
+                    context.pop();
+                  } else if (state is AddMatchFailure) {
+                    showToast(
+                      context: context,
+                      title: state.message,
+                      type: ToastificationType.error,
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return SizedBox(
+                    width: double.infinity,
+                    child: CustomButton(
+                      title: 'Add',
+                      color: AppColors.highlight,
+                      isLoading: state is AddMatchLoading,
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          final match = MatchModel.fromAddMatchInput(
+                            _addMatchInput,
+                          );
+                          context.read<AddMatchCubit>().addMatch(match);
+                        } else {
+                          _autovalidateMode.value = AutovalidateMode.always;
+                        }
+                      },
+                    ),
+                  );
+                },
               )
             ],
           ),
