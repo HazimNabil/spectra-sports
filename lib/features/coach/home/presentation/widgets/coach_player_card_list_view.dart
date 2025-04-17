21c07b383d71456360aa5e2f67caa1523228794a
HazimNabil <HazemNabil95@outlook.com>
2025-04-17 20:34:11 +0200

    Integrate get team cubit with coach players section

--- lib/features/coach/home/presentation/widgets/coach_player_card_list_view.dart
+++ lib/features/coach/home/presentation/widgets/coach_player_card_list_view.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:spectra_sports/core/models/player.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/coach_player_card.dart';
 
 class CoachPlayerCardListView extends StatelessWidget {
-  const CoachPlayerCardListView({super.key});
+  final List<Player> players;
+
+  const CoachPlayerCardListView({super.key, required this.players});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (_, __) => const CoachPlayerCard(),
+      itemCount: players.length,
+      itemBuilder: (_, index) {
+        return CoachPlayerCard(
+          player: players[index],
+        );
+      },
     );
   }
 }
