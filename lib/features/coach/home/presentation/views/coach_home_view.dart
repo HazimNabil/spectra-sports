afd30ee0bb2cee422e212211f0d800a47071942c
HazimNabil <HazemNabil95@outlook.com>
2025-04-17 22:12:53 +0200

    Provide coach matches cubit to coach matches section

--- lib/features/coach/home/presentation/views/coach_home_view.dart
+++ lib/features/coach/home/presentation/views/coach_home_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spectra_sports/core/di/service_locator.dart';
 import 'package:spectra_sports/core/widgets/custom_app_bar.dart';
 import 'package:spectra_sports/core/widgets/custom_tab_bar.dart';
+import 'package:spectra_sports/features/coach/home/data/repos/coach_home_repo_impl.dart';
+import 'package:spectra_sports/features/coach/home/presentation/view_models/coach_matches_cubit/coach_matches_cubit.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/attendance_section.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/coach_matches_section.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/coach_players_section.dart';
@@ -10,26 +14,31 @@ class CoachHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
+    return DefaultTabController(
       length: 3,
       child: Scaffold(
         body: Column(
           children: [
-            CustomAppBar(
+            const CustomAppBar(
               title: 'Team 2010',
               showBackButton: false,
             ),
-            SizedBox(height: 8),
-            CustomTabBar(
+            const SizedBox(height: 8),
+            const CustomTabBar(
               tabs: ['Matches', 'Players', 'Attendance'],
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Expanded(
               child: TabBarView(
                 children: [
-                  CoachMatchesSection(),
-                  CoachPlayersSection(),
-                  AttendanceSection(),
+                  BlocProvider(
+                    create: (context) => CoachMatchesCubit(
+                      locator<CoachHomeRepoImpl>(),
+                    )..getMatches(),
+                    child: const CoachMatchesSection(),
+                  ),
+                  const CoachPlayersSection(),
+                  const AttendanceSection(),
                 ],
               ),
             ),
