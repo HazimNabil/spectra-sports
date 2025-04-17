f9bfa0c4c8d9345d8cca64ccea6992369bcb4993
HazimNabil <HazemNabil95@outlook.com>
2025-04-17 14:45:57 +0200

    Provide add match cubit to add match view body

--- lib/features/admin/home/presentation/views/add_match_view.dart
+++ lib/features/admin/home/presentation/views/add_match_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spectra_sports/core/di/service_locator.dart';
+import 'package:spectra_sports/features/admin/home/data/repos/admin_home_repo_impl.dart';
+import 'package:spectra_sports/features/admin/home/presentation/view_models/add_match_cubit/add_match_cubit.dart';
 import 'package:spectra_sports/features/admin/home/presentation/widgets/add_match_view_body.dart';
 
 class AddMatchView extends StatelessWidget {
@@ -9,7 +13,10 @@ class AddMatchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AddMatchViewBody(teamId: teamId),
+      body: BlocProvider(
+        create: (context) => AddMatchCubit(locator<AdminHomeRepoImpl>()),
+        child: AddMatchViewBody(teamId: teamId),
+      ),
     );
   }
 }
