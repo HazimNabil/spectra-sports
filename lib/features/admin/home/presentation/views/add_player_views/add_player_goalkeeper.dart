47a98ac278102d5d07744cbda8b51c727db8724d
HazimNabil <HazemNabil95@outlook.com>
2025-04-19 03:22:08 +0200

    Use form key to validate add player goalkeeper page

--- lib/features/admin/home/presentation/views/add_player_views/add_player_goalkeeper.dart
+++ lib/features/admin/home/presentation/views/add_player_views/add_player_goalkeeper.dart
@@ -3,28 +3,56 @@ import 'package:spectra_sports/core/utils/app_colors.dart';
 import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/features/admin/home/presentation/widgets/goalkeeper_specs_section.dart';
 
-class AddPlayerGoalkeeper extends StatelessWidget {
+class AddPlayerGoalkeeper extends StatefulWidget {
   final void Function() onNext;
   const AddPlayerGoalkeeper({required this.onNext, super.key});
 
   @override
+  State<AddPlayerGoalkeeper> createState() => _AddPlayerGoalkeeperState();
+}
+
+class _AddPlayerGoalkeeperState extends State<AddPlayerGoalkeeper> {
+  late final GlobalKey<FormState> _formKey;
+  late final AutovalidateMode _autovalidateMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _autovalidateMode = AutovalidateMode.disabled;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          const GoalkeeperSpecsSection(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: CustomButton(
-                title: "Next",
-                onPressed: () => onNext(),
-                color: AppColors.highlight,
+      child: Form(
+        key: _formKey,
+        autovalidateMode: _autovalidateMode,
+        child: Column(
+          children: [
+            const GoalkeeperSpecsSection(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: CustomButton(
+                  title: "Next",
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      widget.onNext();
+                    } else {
+                      setState(() {
+                        _autovalidateMode = AutovalidateMode.always;
+                      });
+                    }
+                  },
+                  color: AppColors.highlight,
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
