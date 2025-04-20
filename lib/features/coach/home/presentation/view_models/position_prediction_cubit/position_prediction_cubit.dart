d37f1fc1b49bead9516476c8c247fc2b05219410
HazimNabil <HazemNabil95@outlook.com>
2025-04-20 10:59:21 +0200

    Create predict position cubit method

--- lib/features/coach/home/presentation/view_models/position_prediction_cubit/position_prediction_cubit.dart
+++ lib/features/coach/home/presentation/view_models/position_prediction_cubit/position_prediction_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spectra_sports/features/coach/home/data/models/predict_position_input.dart';
+import 'package:spectra_sports/features/coach/home/data/repos/coach_home_repo.dart';
 
 part 'position_prediction_state.dart';
 
 class PositionPredictionCubit extends Cubit<PositionPredictionState> {
-  PositionPredictionCubit() : super(const PositionPredictionInitial());
+  final CoachHomeRepo _coachHomeRepo;
+
+  PositionPredictionCubit(this._coachHomeRepo) : super(const PositionPredictionInitial());
+
+  Future<void> predictPosition(PredictPositionInput input) async {
+    emit(const PositionPredictionLoading());
+
+    final positionEither = await _coachHomeRepo.predictPosition(input);
+    positionEither.fold(
+      (failure) => emit(PositionPredictionFailure(failure.message)),
+      (position) => emit(PositionPredictionSuccess(position)),
+    );
+  }
 }
