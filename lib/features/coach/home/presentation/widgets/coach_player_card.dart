19c6881b15744f9f14339cbbf8b59bce80f2dc5a
HazimNabil <HazemNabil95@outlook.com>
2025-04-20 11:10:41 +0200

    Integrate position prediction cubit

--- lib/features/coach/home/presentation/widgets/coach_player_card.dart
+++ lib/features/coach/home/presentation/widgets/coach_player_card.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spectra_sports/core/models/player.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
 import 'package:spectra_sports/core/utils/app_images.dart';
 import 'package:spectra_sports/core/utils/app_styles.dart';
+import 'package:spectra_sports/core/utils/functions.dart';
+import 'package:spectra_sports/features/coach/home/presentation/view_models/position_prediction_cubit/position_prediction_cubit.dart';
+import 'package:toastification/toastification.dart';
 
 class CoachPlayerCard extends StatelessWidget {
   final Player player;
@@ -29,15 +33,35 @@ class CoachPlayerCard extends StatelessWidget {
             context,
           ).copyWith(color: AppColors.icons),
         ),
-        trailing: buildPlayerPositionWidget(context),
+        trailing:
+            BlocConsumer<PositionPredictionCubit, PositionPredictionState>(
+          listener: (context, state) {
+            if (state is PositionPredictionFailure) {
+              showToast(
+                context: context,
+                title: state.message,
+                type: ToastificationType.error,
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is PositionPredictionLoading) {
+              return const CircularProgressIndicator();
+            } else if (state is PositionPredictionSuccess) {
+              return buildPlayerPositionWidget(context, state.position);
+            } else {
+              return buildPlayerPositionWidget(context, player.clubPosition);
+            }
+          },
+        ),
       ),
     );
   }
 
-  Widget buildPlayerPositionWidget(BuildContext context) {
-    if (player.clubPosition != null) {
+  Widget buildPlayerPositionWidget(BuildContext context, String? position) {
+    if (position != null) {
       return Text(
-        player.clubPosition!,
+        position,
         style: AppStyles.styleSemiBold16(
           context,
         ).copyWith(color: AppColors.icons),
