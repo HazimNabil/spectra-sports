b7b8928003c395655748e4cc3bfa47828c022bd7
HazimNabil <HazemNabil95@outlook.com>
2025-04-20 12:16:25 +0200

    Create take attendance cubit method

--- lib/features/coach/home/presentation/view_models/attendance_cubit/attendance_cubit.dart
+++ lib/features/coach/home/presentation/view_models/attendance_cubit/attendance_cubit.dart
@@ -1,9 +1,24 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spectra_sports/features/coach/home/data/models/attendee/attendee.dart';
+import 'package:spectra_sports/features/coach/home/data/repos/coach_home_repo.dart';
 
 part 'attendance_state.dart';
 
 class AttendanceCubit extends Cubit<AttendanceState> {
-  AttendanceCubit() : super(const AttendanceInitial());
+  final CoachHomeRepo _coachHomeRepo;
+
+  AttendanceCubit(this._coachHomeRepo) : super(const AttendanceInitial());
+
+  Future<void> takeAttendance(File image) async {
+    emit(const AttendanceLoading());
+
+    final attendanceEither = await _coachHomeRepo.takeAttendance(image);
+    attendanceEither.fold(
+      (failure) => emit(AttendanceFailure(failure.message)),
+      (attendance) => emit(AttendanceSuccess(attendance)),
+    );
+  }
 }
