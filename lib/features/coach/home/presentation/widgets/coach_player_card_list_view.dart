835474c5a6fd6f90757751320d5abf8c5d0593e2
HazimNabil <HazemNabil95@outlook.com>
2025-04-20 11:02:32 +0200

    Provide position prediction cubit

--- lib/features/coach/home/presentation/widgets/coach_player_card_list_view.dart
+++ lib/features/coach/home/presentation/widgets/coach_player_card_list_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spectra_sports/core/di/service_locator.dart';
 import 'package:spectra_sports/core/models/player.dart';
+import 'package:spectra_sports/features/coach/home/data/repos/coach_home_repo_impl.dart';
+import 'package:spectra_sports/features/coach/home/presentation/view_models/position_prediction_cubit/position_prediction_cubit.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/coach_player_card.dart';
 
 class CoachPlayerCardListView extends StatelessWidget {
@@ -13,8 +17,13 @@ class CoachPlayerCardListView extends StatelessWidget {
       padding: EdgeInsets.zero,
       itemCount: players.length,
       itemBuilder: (_, index) {
-        return CoachPlayerCard(
-          player: players[index],
+        return BlocProvider(
+          create: (context) => PositionPredictionCubit(
+            locator<CoachHomeRepoImpl>(),
+          ),
+          child: CoachPlayerCard(
+            player: players[index],
+          ),
         );
       },
     );
