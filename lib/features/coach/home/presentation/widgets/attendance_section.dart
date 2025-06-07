be83565392db6b46c2699633573905f4e3fdec13
HazimNabil <HazemNabil95@outlook.com>
2025-06-07 03:57:45 +0300

    Make attendance section persistent

--- lib/features/coach/home/presentation/widgets/attendance_section.dart
+++ lib/features/coach/home/presentation/widgets/attendance_section.dart
@@ -4,15 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:spectra_sports/core/utils/app_colors.dart';
+import 'package:spectra_sports/core/utils/app_styles.dart';
 import 'package:spectra_sports/core/widgets/custom_button.dart';
 import 'package:spectra_sports/features/coach/home/presentation/view_models/attendance_cubit/attendance_cubit.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/attendance_card_list_view.dart';
 
-class AttendanceSection extends StatelessWidget {
+class AttendanceSection extends StatefulWidget {
   const AttendanceSection({super.key});
 
   @override
+  State<AttendanceSection> createState() => _AttendanceSectionState();
+}
+
+class _AttendanceSectionState extends State<AttendanceSection>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Column(
       spacing: 16,
       children: [
@@ -30,7 +41,14 @@ class AttendanceSection extends StatelessWidget {
                 AttendanceFailure(message: final message) => Center(
                     child: Text(message),
                   ),
-                _ => const Text('You haven\'t taken any attendance yet'),
+                _ => Center(
+                    child: Text(
+                      'You haven\'t taken any attendance yet',
+                      style: AppStyles.styleBold15(context).copyWith(
+                        color: AppColors.text,
+                      ),
+                    ),
+                  ),
               };
             },
           ),
@@ -47,8 +65,9 @@ class AttendanceSection extends StatelessWidget {
                   await imagePicker.pickImage(source: ImageSource.gallery);
               File? file;
               if (image != null) file = File(image.path);
-              if (file != null)
-                context.read<AttendanceCubit>().takeAttendance(file);
+              if (file != null && context.mounted) {
+                await context.read<AttendanceCubit>().takeAttendance(file);
+              }
             },
           ),
         ),
