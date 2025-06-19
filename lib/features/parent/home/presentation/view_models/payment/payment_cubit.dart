d97a1546114ff47506a561b208694c89e83a7d15
HazimNabil <HazemNabil95@outlook.com>
2025-06-20 01:43:21 +0300

    Add pay fees method in payment cubit

--- lib/features/parent/home/presentation/view_models/payment/payment_cubit.dart
+++ lib/features/parent/home/presentation/view_models/payment/payment_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:spectra_sports/features/parent/home/data/repos/parent_home_repo.dart';
 
 part 'payment_state.dart';
 
 class PaymentCubit extends Cubit<PaymentState> {
-  PaymentCubit() : super(const PaymentInitial());
+  final ParentHomeRepo _parentHomeRepo;
+
+  PaymentCubit(this._parentHomeRepo) : super(const PaymentInitial());
+
+  Future<void> payFees(double amount) async {
+    emit(const PaymentLoading());
+
+    final result = await _parentHomeRepo.payFees(amount);
+
+    result.fold(
+      (failure) => emit(PaymentFailure(failure.message)),
+      (success) => emit(const PaymentSuccess()),
+    );
+  }
 }
