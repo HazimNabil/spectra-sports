48fdffdf0afc434b10600da94eac75901d62dbb2
HazimNabil <HazemNabil95@outlook.com>
2025-06-23 01:24:03 +0300

    Create shot card widget

--- lib/features/coach/home/presentation/widgets/shot_analysis_section.dart
+++ lib/features/coach/home/presentation/widgets/shot_analysis_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spectra_sports/features/coach/home/presentation/widgets/shot_card.dart';
 
 class ShotAnalysisSection extends StatelessWidget {
   const ShotAnalysisSection({super.key});
@@ -6,7 +7,9 @@ class ShotAnalysisSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [],
+      children: [
+        ShotCard(),
+      ],
     );
   }
 }
