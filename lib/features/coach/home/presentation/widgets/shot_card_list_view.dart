2b5ecfc14cd3f939b64e61ef0a92df9a012144bf
HazimNabil <HazemNabil95@outlook.com>
2025-06-23 02:32:36 +0300

    Add stat tile to shot card list view

--- lib/features/coach/home/presentation/widgets/shot_card_list_view.dart
+++ lib/features/coach/home/presentation/widgets/shot_card_list_view.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:spectra_sports/features/coach/home/presentation/widgets/shot_card.dart';
+import 'package:spectra_sports/features/coach/home/presentation/widgets/stat_tile.dart';
 
 class ShotCardListView extends StatelessWidget {
+  final shotStats = const [
+    (title: 'Total Shots', value: '10'),
+    (title: 'Total Goals', value: '7'),
+    (title: 'Goals Probability', value: '70%'),
+  ];
+
   const ShotCardListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (_, index) {
-        return const ShotCard();
-      },
+    return ListView(
+      children: [
+        ...List.generate(
+          10,
+          (index) => const ShotCard(),
+        ),
+        const SizedBox(height: 8),
+        Divider(
+          color: Colors.grey[400],
+          indent: 16,
+          endIndent: 16,
+          thickness: 1.5,
+        ),
+        ...List.generate(
+          shotStats.length,
+          (index) {
+            final stat = shotStats[index];
+            return StatTile(stat: stat);
+          },
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/coach/home/presentation/widgets/stat_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:spectra_sports/core/utils/app_colors.dart';
+import 'package:spectra_sports/core/utils/app_styles.dart';
+
+class StatTile extends StatelessWidget {
+  final ({String title, String value}) stat;
+
+  const StatTile({super.key, required this.stat});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        stat.title,
+        style: AppStyles.styleSemiBold16(context).copyWith(
+          color: AppColors.text,
+        ),
+      ),
+      trailing: Text(
+        stat.value,
+        style: AppStyles.styleSemiBold16(context),
+      ),
+    );
+  }
+}
