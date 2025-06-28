330e0332ca266d623517c15e4cba26f5ab162185
HazimNabil <HazemNabil95@outlook.com>
2025-06-28 13:13:42 +0300

    Create shot analysis cubit method

--- lib/features/coach/home/presentation/view_models/shot_analysis_cubit/shot_analysis_cubit.dart
+++ lib/features/coach/home/presentation/view_models/shot_analysis_cubit/shot_analysis_cubit.dart
@@ -1,9 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:spectra_sports/features/coach/home/data/models/shot_analysis/shot_analysis.dart';
+import 'package:spectra_sports/features/coach/home/data/repos/coach_home_repo.dart';
 
 part 'shot_analysis_state.dart';
 
 class ShotAnalysisCubit extends Cubit<ShotAnalysisState> {
-  ShotAnalysisCubit() : super(const ShotAnalysisInitial());
+  final CoachHomeRepo coachHomeRepo;
+
+  ShotAnalysisCubit(this.coachHomeRepo) : super(const ShotAnalysisInitial());
+
+  Future<void> analyzeShots(File video) async {
+    emit(const ShotAnalysisLoading());
+
+    final shotAnalysisEither = await coachHomeRepo.analyzeShots(video);
+    shotAnalysisEither.fold(
+      (failure) => emit(ShotAnalysisFailure(failure.message)),
+      (shotAnalysis) => emit(ShotAnalysisSuccess(shotAnalysis)),
+    );
+  }
 }
